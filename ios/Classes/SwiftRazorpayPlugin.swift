d6ad7991aa7b86b6ce87367dbcbac5f37a45ae6d
mohansiriga7 <ms7.developer@gmail.com>
2019-05-21 18:47:08 +0530

    Handling order_id and signature of Razorpay response

--- ios/Classes/SwiftRazorpayPlugin.swift
+++ ios/Classes/SwiftRazorpayPlugin.swift
@@ -1,7 +1,7 @@
 import Flutter
 import UIKit
 import Razorpay
-public class SwiftRazorpayPlugin: NSObject, FlutterPlugin, RazorpayPaymentCompletionProtocol  {
+public class SwiftRazorpayPlugin: NSObject, FlutterPlugin, RazorpayPaymentCompletionProtocolWithData  {
 
   var razorpay: Razorpay!
   var _result: FlutterResult!
@@ -25,11 +25,14 @@ public class SwiftRazorpayPlugin: NSObject, FlutterPlugin, RazorpayPaymentComple
             let prefill_contact = argue!["contact"] as? String
             let product_theme = argue!["theme"] as? String
             let API_KEY = argue!["api_key"] as? String
-            razorpay = Razorpay.initWithKey(API_KEY!, andDelegate: self )
-            showPaymentForm(name: product_name!, des:product_des!, image:product_image!, amount:product_amount!,email:prefill_email!, contact:prefill_contact!, theme:product_theme!);
+            let order_id = argue!["order_id"] as? String
+            let prefill_method = argue!["method"] as? String
+            let notes = argue!["notes"] as? NSDictionary
+            razorpay = Razorpay.initWithKey(API_KEY!, andDelegateWithData: self )
+            showPaymentForm(name: product_name!, des:product_des!, image:product_image!, amount:product_amount!,email:prefill_email!, contact:prefill_contact!, theme:product_theme!, notes: notes, order_id: order_id!, method:prefill_method!);
         }
     }
-    public func showPaymentForm(name:String,des:String,image:String,amount:String,email:String,contact:String,theme:String)
+    public func showPaymentForm(name:String,des:String,image:String,amount:String,email:String,contact:String,theme:String, notes: NSDictionary?, order_id:String, method:String)
     {
         let params: [String:Any] =
             [
@@ -37,28 +40,36 @@ public class SwiftRazorpayPlugin: NSObject, FlutterPlugin, RazorpayPaymentComple
                 "description": des,
                 "image": image,
                 "amount": amount,
+                "order_id": order_id,
                 "prefill":[
                     "contact": contact,
                     "email": email,
+                    "method": method
                 ],
                 "theme":[
                     "color":theme
                 ],
+                "notes": notes ?? [:]
             ]
         razorpay.open(params)
     }
-    public func onPaymentError(_ code: Int32, description str: String)
+    public func onPaymentError(_ code: Int32, description str: String, andData response: [AnyHashable : Any]?)
     {
         var response = [String : String]()
-        response["message"] = str
         response["code"] = "0"
+        response["message"] = str
         _result(response)
     }
     
-    public func onPaymentSuccess(_ payment_id: String)
+    public func onPaymentSuccess(_ payment_id: String, andData response: [AnyHashable : Any]?)
     {
+    NSLog("RAZORPAY PAYMENT SUCCESS")
+    let signature = response!["razorpay_signature"] as? String
+    let razorpay_payment_id = response!["razorpay_payment_id"] as? String
+
         var response = [String : String]()
-        response["message"] = payment_id
+        response["message"] = razorpay_payment_id
+        response["signature"] = signature
         response["code"] = "1"
         _result(response)
     }
